12cfd1ce11c80a69c81f098d098069942548ddd2
Yunis <yunis@88.com>
2026-04-23 15:08:00 +0800

    chore: remove unnecessary environment variable from Windows build and update main.rs to hide console window in release mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 #[cfg(not(target_os = "linux"))]
 use gpui::TitlebarOptions;
 use gpui::{
